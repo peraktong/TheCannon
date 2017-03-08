2d2eed23ce8c5fdeb59b1cb4d9f072e06e142220
Jason Cao <jc6933@nyu.edu>
2017-03-07 21:40:24 -0500

    Define _cont_norm inside the dataset

--- TheCannon/dataset.py
+++ TheCannon/dataset.py
@@ -581,3 +581,73 @@ def convert_to_hdulist(ds):
 
     print('@Bo Zhang: ---------------------------------------------------')
     return fits.HDUList(hl)
+
+
+def _cont_norm(fluxes, ivars, cont):
+    """ Continuum-normalize a continuous segment of spectra.
+
+    Parameters
+    ----------
+    fluxes: numpy ndarray 
+        pixel intensities
+    ivars: numpy ndarray 
+        inverse variances, parallel to fluxes
+    contmask: boolean mask
+        True indicates that pixel is continuum
+
+    Returns
+    -------
+    norm_fluxes: numpy ndarray
+        normalized pixel intensities
+    norm_ivars: numpy ndarray
+        rescaled inverse variances
+    """
+    nstars = fluxes.shape[0]
+    npixels = fluxes.shape[1]
+    norm_fluxes = np.ones(fluxes.shape)
+    norm_ivars = np.zeros(ivars.shape)
+    bad = cont == 0.
+    norm_fluxes = np.ones(fluxes.shape)
+    norm_fluxes[~bad] = fluxes[~bad] / cont[~bad]
+    norm_ivars = cont**2 * ivars
+    return norm_fluxes, norm_ivars 
+
+
+def _cont_norm_regions(fluxes, ivars, cont, ranges):
+    """ Perform continuum normalization for spectra in chunks
+
+    Useful for spectra that have gaps
+
+    Parameters
+    ---------
+    fluxes: numpy ndarray
+        pixel intensities
+    ivars: numpy ndarray
+        inverse variances, parallel to fluxes
+    cont: numpy ndarray
+        the continuum
+    ranges: list or np ndarray
+        the chunks that the spectrum should be split into
+
+    Returns
+    -------
+    norm_fluxes: numpy ndarray
+        normalized pixel intensities
+    norm_ivars: numpy ndarray
+        rescaled inverse variances
+    """
+    nstars = fluxes.shape[0]
+    norm_fluxes = np.zeros(fluxes.shape)
+    norm_ivars = np.zeros(ivars.shape)
+    for chunk in ranges:
+        start = chunk[0]
+        stop = chunk[1]
+        output = _cont_norm(fluxes[:,start:stop],
+                           ivars[:,start:stop],
+                           cont[:,start:stop])
+        norm_fluxes[:,start:stop] = output[0]
+        norm_ivars[:,start:stop] = output[1]
+    for jj in range(nstars):
+        bad = (norm_ivars[jj,:] == 0.)
+        norm_fluxes[jj,:][bad] = 1.
+    return norm_fluxes, norm_ivars
